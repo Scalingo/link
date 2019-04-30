5c12a4ad9e8bc6dba3649f52457a0ac7bc14a5e7
Soulou <leo@unbekandt.eu>
2019-04-30 15:53:33 +0200

    Update healthcheck logics to use new Error from Philae

--- healthcheck/checker.go
+++ healthcheck/checker.go
@@ -2,14 +2,12 @@ package healthcheck
 
 import (
 	"context"
-	"io/ioutil"
 
 	"github.com/Scalingo/go-philae/prober"
 	"github.com/Scalingo/go-philae/tcpprobe"
 	"github.com/Scalingo/go-utils/logger"
 	"github.com/Scalingo/link/config"
 	"github.com/Scalingo/link/models"
-	"github.com/sirupsen/logrus"
 )
 
 type Checker interface {
@@ -36,22 +34,12 @@ func FromChecks(config config.Config, checks []models.Healthcheck) checker {
 }
 
 func (c checker) IsHealthy(ctx context.Context) bool {
-	appLogger := logger.Get(ctx)
+	log := logger.Get(ctx)
 
-	// Custom logger to discard Philae output
-	log := logrus.New()
-	log.Out = ioutil.Discard
-	philaeCtx := logger.ToCtx(context.Background(), log)
+	res := c.prober.Check(ctx)
+	if res.Error != nil {
+		log.WithError(res.Error).Error("Healthcheck failed")
 
-	res := c.prober.Check(philaeCtx)
-	if !res.Healthy {
-		var reasons []string
-		for _, probe := range res.Probes {
-			if !probe.Healthy {
-				reasons = append(reasons, probe.Comment)
-			}
-		}
-		appLogger.WithField("reasons", reasons).Error("Probe failed")
 	}
 	return res.Healthy
 }
