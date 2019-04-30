3dfe6300091ab5fedd3bc615b4fe6d612e8b696c
Soulou <leo@unbekandt.eu>
2019-04-30 15:43:06 +0200

    Merge go-philae v4.3.0

--- vendor/github.com/Scalingo/go-philae/prober/prober.go
+++ vendor/github.com/Scalingo/go-philae/prober/prober.go
@@ -2,10 +2,11 @@ package prober
 
 import (
 	"context"
-	"errors"
+	"fmt"
+	"strings"
 	"time"
 
-	"github.com/Scalingo/go-utils/logger"
+	"gopkg.in/errgo.v1"
 )
 
 // Probe define a minimal set of methods that a probe should implement
@@ -17,24 +18,70 @@ type Probe interface {
 // Prober entrypoint of the philae api. It will retain a set of probe and run
 // checks when asked to
 type Prober struct {
-	probes []Probe
+	timeout time.Duration
+	probes  []Probe
+}
+
+// ProberOption is a function modifying some parameters of the Prober
+type ProberOption func(p *Prober)
+
+// WithTimeout is a ProberOption which defines a timeout the prober have to get
+// executed into Recommandation: it should be higher than the timeout of the
+// probes included in it, otherwise it would mask the real errors.
+func WithTimeout(d time.Duration) ProberOption {
+	return ProberOption(func(p *Prober) {
+		p.timeout = d
+	})
+}
+
+type ProberError struct {
+	errs []string
+}
+
+func (e *ProberError) AddError(name string, err error) {
+	msg := fmt.Sprintf("probe %s: %s", name, err.Error())
+	e.errs = append(e.errs, msg)
+}
+
+func (e *ProberError) IsEmpty() bool {
+	return e.errs == nil
+}
+
+func (e *ProberError) Error() string {
+	if e.errs == nil {
+		return "no error"
+	}
+	b := strings.Builder{}
+	b.WriteString("prober error: ")
+	b.WriteString(strings.Join(e.errs, ", "))
+	return b.String()
 }
 
 // Result is the data structure used to retain the data fetched from a single run of each probes
 type Result struct {
 	Healthy bool           `json:"healthy"`
 	Probes  []*ProbeResult `json:"probes"`
+	Error   error          `json:"error"`
 }
 
 // ProbeResult is the data structure used to retain the data fetched from a single probe
 type ProbeResult struct {
-	Name    string `json:"name"`
-	Healthy bool   `json:"healthy"`
-	Comment string `json:"comment"`
+	Name     string        `json:"name"`
+	Healthy  bool          `json:"healthy"`
+	Comment  string        `json:"comment"`
+	Error    error         `json:"error"`
+	Duration time.Duration `json:"duration"`
 }
 
-func NewProber() *Prober {
-	return &Prober{}
+// NewProber is the default constructor of a Prober
+func NewProber(opts ...ProberOption) *Prober {
+	p := &Prober{
+		timeout: 10 * time.Second,
+	}
+	for _, opt := range opts {
+		opt(p)
+	}
+	return p
 }
 
 func (p *Prober) AddProbe(probe Probe) {
@@ -44,8 +91,13 @@ func (p *Prober) AddProbe(probe Probe) {
 // Check will run the check of each probes added and return the result in a Result struct
 func (p *Prober) Check(ctx context.Context) *Result {
 	probesResults := make([]*ProbeResult, len(p.probes))
-	healthy := true
 	resultChan := make(chan *ProbeResult, len(p.probes))
+	healthy := true
+	err := &ProberError{}
+
+	ctx, cancel := context.WithTimeout(ctx, p.timeout)
+	defer cancel()
+
 	for _, probe := range p.probes {
 		go p.CheckOneProbe(ctx, probe, resultChan)
 	}
@@ -54,43 +106,50 @@ func (p *Prober) Check(ctx context.Context) *Result {
 		probeResult := <-resultChan
 		if !probeResult.Healthy {
 			healthy = false
+			err.AddError(probeResult.Name, probeResult.Error)
 		}
 		probesResults[i] = probeResult
+	}
+
+	if err.IsEmpty() {
+		err = nil
 	}
 
 	return &Result{
 		Healthy: healthy,
 		Probes:  probesResults,
+		Error:   err,
 	}
 }
 
 func (p *Prober) CheckOneProbe(ctx context.Context, probe Probe, res chan *ProbeResult) {
-	log := logger.Get(ctx)
 	probeRes := make(chan error)
 	var err error
 
-	timer := time.NewTimer(2 * time.Second)
-
+	begin := time.Now()
 	go ProberWrapper(probe, probeRes)
 
 	select {
 	case e := <-probeRes:
 		err = e
-	case <-timer.C:
-		err = errors.New("Probe timeout")
+	case <-ctx.Done():
+		err = fmt.Errorf("prober: %s", ctx.Err())
 	}
 
 	probe_healthy := true
-	comment := ""
+	duration := time.Now().Sub(begin)
+	comment := fmt.Sprintf("took %v", duration)
 	if err != nil {
-		comment = err.Error()
+		err = errgo.Notef(err, "probe check failed")
+		comment = "error"
 		probe_healthy = false
-		log.Infof("[PHILAE] Probe %s failed, reason: %s\n", probe.Name(), err.Error())
 	}
 	probeResult := &ProbeResult{
-		Name:    probe.Name(),
-		Healthy: probe_healthy,
-		Comment: comment,
+		Name:     probe.Name(),
+		Healthy:  probe_healthy,
+		Comment:  comment,
+		Error:    err,
+		Duration: duration,
 	}
 
 	res <- probeResult
--- vendor/github.com/Scalingo/go-philae/tcpprobe/tcpprobe.go
+++ vendor/github.com/Scalingo/go-philae/tcpprobe/tcpprobe.go
@@ -1,11 +1,21 @@
 package tcpprobe
 
 import (
+	"context"
+	"fmt"
 	"net"
 	"time"
 
 	errgo "gopkg.in/errgo.v1"
 )
+
+type Resolver interface {
+	LookupIPAddr(context.Context, string) ([]net.IPAddr, error)
+}
+
+type Dialer interface {
+	DialContext(context.Context, string, string) (net.Conn, error)
+}
 
 type TCPProbe struct {
 	name     string
@@ -14,10 +24,18 @@ type TCPProbe struct {
 }
 
 type TCPOptions struct {
-	Timeout time.Duration
+	Timeout  time.Duration
+	Resolver Resolver
+	Dialer   Dialer
 }
 
 func NewTCPProbe(name, endpoint string, opts TCPOptions) TCPProbe {
+	if opts.Resolver == nil {
+		opts.Resolver = net.DefaultResolver
+	}
+	if opts.Dialer == nil {
+		opts.Dialer = &net.Dialer{}
+	}
 	return TCPProbe{
 		name:     name,
 		endpoint: endpoint,
@@ -30,18 +48,37 @@ func (p TCPProbe) Name() string {
 }
 
 func (p TCPProbe) Check() error {
-	dialer := &net.Dialer{}
 	timeout := p.options.Timeout
 	if timeout == 0 {
 		timeout = 5 * time.Second
 	}
-	dialer.Timeout = timeout
 
-	a, err := dialer.Dial("tcp", p.endpoint)
+	host, port, err := net.SplitHostPort(p.endpoint)
 	if err != nil {
-		return errgo.Notef(err, "fail to contact endpoint")
+		return errgo.Notef(err, "invalid endpoint %v, should be host:port", p.endpoint)
 	}
 
-	a.Close()
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	addrs, err := p.options.Resolver.LookupIPAddr(ctx, host)
+	if err != nil {
+		return errgo.Notef(err, "DNS resolution failed for %v", host)
+	}
+
+	endpoint := addrs[0].String() + ":" + port
+	if len(addrs[0].IP) == net.IPv6len {
+		endpoint = fmt.Sprintf("[%s]:%s", addrs[0].String(), port)
+	}
+
+	a, err := p.options.Dialer.DialContext(ctx, "tcp", endpoint)
+	if err != nil {
+		return errgo.Notef(err, "fail to open TCP connection")
+	}
+
+	err = a.Close()
+	if err != nil {
+		return errgo.Notef(err, "fail to close connection")
+	}
 	return nil
 }
