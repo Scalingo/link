f2b6d87e4c5613e4afb1ea26f2e14042f486624e
Jonathan Hurter <john@scalingo.com>
2019-09-02 15:25:37 +0200

    Keep IP on restart

--- models/storage.go
+++ models/storage.go
@@ -5,5 +5,6 @@ import "context"
 type Storage interface {
 	GetIPs(context.Context) ([]IP, error)
 	AddIP(context.Context, IP) (IP, error)
+	UpdateIP(ctx context.Context, ip IP) error
 	RemoveIP(context.Context, string) error
 }
